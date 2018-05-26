c8ed142ce76c779bd2eac2a0549e4cf2f0956452
Nathalia Cibirka de Oliveira <ncibirka@nathalias-mbp.mynet>
2018-05-26 16:16:44 +0300

    MLcommit

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -7,8 +7,22 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
+
+z2 = X * transpose(Theta1);
+a2 = sigmoid(z2);
+
+a2 = [ones(size(a2,1),1) a2];
+
+z3 = a2 * transpose(Theta2);
+a3 = sigmoid(z3);
+
+[I,M] = max(a3,[],2);
+p = M;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
@@ -20,13 +34,6 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
-
-
-
-
-
-
-
 
 
 % =========================================================================
